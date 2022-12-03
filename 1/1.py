8556209d1e213e5c13c839685175ce829bc6ac2f
Zhag Magauiana <zhag.magauina@freenome.com>
2022-12-03 15:00:19 -0800

    Refactor day 1

--- 1/1.py
+++ 1/1.py
@@ -2,24 +2,17 @@ with open("input.txt") as f:
     input = f.read()
 
 def part_1(input):
-    lines = input.splitlines()
-    for i in range(len(lines)):
-        lines[i] = int(lines[i]) if lines[i] else lines[i]
-    max = 0
-    curr = 0
-    for item in lines:
-        if item == '':
-            if curr > max:
-                max = curr
-            curr = 0
-        else:
-            curr = curr + item
+    groups = input.split('\n\n')
+    for group in groups:
+        total = sum([int(item) for item in group.split('\n')])
+        if total > max:
+            max = total
     return max
 
 print('Part 1: ', part_1(input))
 
 
-def first_try_2(input):
+def part_2(input):
     groups = input.split('\n\n')
     totals = []
     for group in groups:
