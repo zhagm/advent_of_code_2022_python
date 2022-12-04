9fda351cee62960afa8b227528b5ab1b17fe4ca9
Zhag Magauiana <zhag.magauina@freenome.com>
2022-12-03 16:26:50 -0800

    delete comments

--- 2/solution.py
+++ 2/solution.py
@@ -1,17 +1,6 @@
 with open("input.txt") as f:
     input = f.read().splitlines()
 
-# A for Rock, B for Paper, and C for Scissors
-# X for Rock, Y for Paper, and Z for Scissors
-# Your total score is the sum of your scores for each round.
-# The score for a single round is the score for the shape you selected
-# (1 for Rock, 2 for Paper, and 3 for Scissors) plus the score for the outcome of the round
-# (0 if you lost, 3 if the round was a draw, and 6 if you won)
-
-# rock > scissors
-# scissors > paper
-# paper > rock
-
 def part_1(rounds):
     score = 0
     points_map = { 'X': 1, 'Y': 2, 'Z': 3 }
@@ -21,14 +10,6 @@ def part_1(rounds):
     return score
 
 print('Part 1: ', part_1(input))
-
-# X means you need to lose
-# Y means you need to draw
-# Z means you need to win
-
-# A > C
-# C > B
-# B > A
 
 def part_2(rounds):
     score = 0
