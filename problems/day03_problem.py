62e708c1da671a34ff1b5319d0fbaa13f95f6dea
Zhag Magauiana <zhag.magauina@freenome.com>
2022-12-03 18:22:18 -0800

    day 3, pt 2

--- problems/day03_problem.py
+++ problems/day03_problem.py
@@ -1,25 +1,34 @@
+import string
+
 with open("data/day03_input.txt") as f:
     input = f.read().splitlines()
 
-def get_priority(c):
-    charCode = ord(c)
-    if charCode >= 65 and charCode <= 90:
-        return charCode - 38
-    if charCode >= 97 and charCode <= 122:
-        return charCode - 96
+PRIORITY_INDEX_STRING = '#' + string.ascii_lowercase + string.ascii_uppercase
 
-def part_1(rucksacks):
+def find_common_char(strs):
+    for char in strs[0]:
+        if all(map(lambda s: char in s, strs[1:])):
+            return char
+    return -1
+
+#########################
+
+def part_1(input):
     priority_sum = 0
-    for r in rucksacks:
+    for r in input:
         half_len = len(r)//2
         first, second = r[:half_len], r[half_len:]
-        duplicate = [item for item in first if second.find(item) != -1]
-        priority_sum = priority_sum + get_priority(duplicate[0])
+        duplicate = find_common_char([first, second])
+        priority_sum += PRIORITY_INDEX_STRING.find(duplicate)
     return priority_sum
 
 print('Part 1: ', part_1(input))
 
 def part_2(input):
-    return
+    priority_sum = 0
+    for i in range(0, len(input), 3):
+        duplicate = find_common_char(input[i:i+3])
+        priority_sum += PRIORITY_INDEX_STRING.find(duplicate)
+    return priority_sum
 
 print('Part 2: ', part_2(input))
