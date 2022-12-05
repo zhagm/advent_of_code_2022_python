aaeb6660c816e249fb306e5047e5290d8d2b8758
Zhag Magauiana <zhag.magauina@freenome.com>
2022-12-05 10:54:14 -0800

    refactor day 5

--- problems/day05_problem.py
+++ problems/day05_problem.py
@@ -3,41 +3,42 @@ import re
 with open("data/day05_input.txt") as f:
     input = f.read().splitlines()
 
-def parse_data(input):
-    stacks = [[] for i in range(10)]
-    moves_start = -1
-    for line_index in range(len(input)):
-        line = input[line_index]
-        stack_num = 0
-        if (line[1] == '1'):
-            moves_start = line_index + 2
-            break
-        for item in [line[i] for i in range(1, len(line), 4)]:
-            stack_num += 1
-            if item != ' ':
-                stacks[stack_num].insert(0, item)
+def parse_moves(input):
     moves = []
     digits_pattern = re.compile(r'(\d+)')
-    for line in input[moves_start:]:
+    for line in input:
         moves.append([int(d) for d in digits_pattern.findall(line)])
-    return stacks, moves
+    return moves
 
-STACKS, MOVES = parse_data(input)
+MOVES = parse_moves(input)
+STACKS = [
+    [],
+    ['Z', 'J', 'N', 'W', 'P', 'S'],
+    ['G', 'S', 'T'],
+    ['V', 'Q', 'R', 'L', 'H'],
+    ['V', 'S', 'T', 'D'],
+    ['Q', 'Z', 'T', 'D', 'B', 'M', 'J'],
+    ['M', 'W', 'T', 'J', 'D', 'C', 'Z', 'L'],
+    ['L', 'P', 'M', 'W', 'G', 'T', 'J'],
+    ['N', 'G', 'M', 'T', 'B', 'F', 'Q', 'H'], ['R', 'D', 'G', 'C', 'P', 'B', 'Q', 'W']
+]
 
 ############################################################
 
 def part_1(stacks, moves):
+    stacks = stacks.copy()
     for [count, src, dest] in moves:
-        stacks[dest] = stacks[dest] + stacks[src][-count:][::-1]
+        stacks[dest] += stacks[src][-count:][::-1]
         stacks[src] = stacks[src][:-count]
-    return ''.join([stack[-1] for stack in stacks[1:] if len(stack)])
+    return ''.join([stack[-1] for stack in stacks if len(stack)])
 
-print('Part 1: ', part_1(STACKS.copy(), MOVES))
+print('Part 1: ', part_1(STACKS, MOVES))
 
 def part_2(stacks, moves):
+    stacks = stacks.copy()
     for [count, src, dest] in moves:
-        stacks[dest] = stacks[dest] + stacks[src][-count:]
+        stacks[dest] += stacks[src][-count:]
         stacks[src] = stacks[src][:-count]
-    return ''.join([stack[-1] for stack in stacks[1:] if len(stack)])
+    return ''.join([stack[-1] for stack in stacks if len(stack)])
 
-print('Part 2: ', part_2(STACKS.copy(), MOVES))
+print('Part 2: ', part_2(STACKS, MOVES))
