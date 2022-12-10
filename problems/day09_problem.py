89e2844812302497c06f1fa2d7935027404f7d7f
Zhag Magauiana <zhag.magauina@freenome.com>
2022-12-10 11:52:12 -0800

    day 9, pt 2

--- problems/day09_problem.py
+++ problems/day09_problem.py
@@ -2,7 +2,6 @@ with open("data/day09_input.txt") as f:
     input = f.read().splitlines()
     input = [row.split(' ') for row in input]
     input = [[move[0], int(move[1])] for move in input]
-    print(input)
 
 
 class Point:
@@ -11,11 +10,6 @@ class Point:
         self.x = x
         self.y = y
         self.visited = { F"{x},{y}": True } if track_visited else None
-
-    def is_touching(self, other):
-        if abs(self.x - other.x) <= 1 and abs(self.y - other.y) <= 1:
-            return True
-        return False
     
     def move(self, dir):
         if dir == 'U':
@@ -26,52 +20,49 @@ class Point:
             self.y -= 1
         elif dir == 'L':
             self.x -= 1
-        else:
-            print('ERROR in move', dir)
         if self.visited:
             self.visited[F"{self.x},{self.y}"] = True
 
     def follow(self, other):
-        x_diff = abs(self.x - other.x)
-        y_diff = abs(self.y - other.y)
-        print('x diff', x_diff)
-        print('y diff', y_diff)
-        if x_diff >= 1 and y_diff >= 1:
-            if self.x - other.x < 0:
-                self.x += 1
-            else:
-                self.x -= 1
-            if self.y - other.y < 0:
-                self.y += 1
-            else:
-                self.y -= 1
-        elif x_diff > 1:
-            if self.x - other.x < 0:
-                self.x += 1
-            else:
-                self.x -= 1
-        elif y_diff > 1:
-            if self.y - other.y < 0:
-                self.y += 1
-            else:
-                self.y -= 1
+        x_diff = self.x - other.x
+        x_diff_abs = abs(x_diff)
+        y_diff = self.y - other.y
+        y_diff_abs = abs(y_diff)
+
+        # If they are touching, skip moving
+        if x_diff_abs <= 1 and y_diff_abs <= 1:
+            return
+
+        if x_diff_abs >= 1 and y_diff_abs >= 1:
+            self.x += 1 if x_diff < 0 else -1
+            self.y += 1 if y_diff < 0 else -1
+        elif x_diff_abs > 1:
+            self.x += 1 if x_diff < 0 else -1
+        elif y_diff_abs > 1:
+            self.y += 1 if y_diff < 0 else -1
         if self.visited:
             self.visited[F"{self.x},{self.y}"] = True
         
     
-    def print(self, other):
-        max_x = max(self.x, other.x) + 1
-        max_y = max(self.y, other.y) + 1
+    def print(self, others):
+        max_x = max(self.x, *[o.x for o in others]) + 1
+        max_y = max(self.y,  *[o.y for o in others]) + 1
+        min_x = min(self.x, *[o.x for o in others])
+        min_y = min(self.y,  *[o.y for o in others])
+        grid_x = range(min_x, max_x)
+        grid_y = range(min_y, max_y)[::-1]
 
         grid = []
-        for y in range(max_y)[::-1]:
+        for y in grid_y:
             row = []
-            for x in range(max_x):
-                if self.x == x and self.y == y:
-                    row.append(self.name)
-                elif other.x == x and other.y == y:
-                    row.append(other.name)
-                else:
+            for x in grid_x:
+                printed = False
+                for pt in [self, *others]:
+                    if pt.x == x and pt.y == y:
+                        row.append(pt.name)
+                        printed = True
+                        break
+                if not printed:
                     row.append('.')
             grid.append(row)
         print('\n'.join([''.join(row) for row in grid]), '\n')
@@ -81,21 +72,24 @@ def part_1(input):
     H = Point('H', 0, 0)
     T = Point('T', 0, 0, True)
     for [dir, steps] in input:
-        print('----------- NEW MOVE')
         for i in range(steps):
             H.move(dir)
-            print('-HEAD MOVE-')
-            T.print(H)
-            if not T.is_touching(H):
-                T.follow(H)
-            print('-TAIL MOVE-')
-            T.print(H)
+            T.follow(H)
     return len(T.visited.values())
 
 
 print('Part 1: ', part_1(input))
 
 def part_2(input):
-    return
+    H = Point('H', 0, 0)
+    others = [Point(str(i + 1), 0, 0, True if i == 8 else False) for i in range(9)]
+    for [dir, steps] in input:
+        for i in range(steps):
+            H.move(dir)
+            prev = H
+            for point in others:
+                point.follow(prev)
+                prev = point
+    return len(others[-1].visited)
 
 print('Part 2: ', part_2(input))
